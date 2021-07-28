01762a5e7cb0d1aea704597a948b827cd59c0ac6
abraham perez reyes <abrahampre27@gmail.com>
2021-07-28 14:14:44 -0500

    last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,6 +71,7 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+
 };
 
 // enable draggable/sortable feature on list-group elements
@@ -82,15 +83,23 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event, ui) {
     console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
     console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log(event);
+    $(this).addClass("dropover-active");
+ 
   },
   out: function(event) {
     console.log(event);
+    $(this).removeClass("dropover-active");
+    
   },
   update: function() {
     var tempArr = [];
@@ -136,9 +145,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -161,7 +172,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -288,3 +299,9 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  });
+},(1000*60)*30);
