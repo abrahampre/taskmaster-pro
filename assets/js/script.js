0b85dc6daae4d40fa7de3bf75fa5ec76f6870c14
abraham perez reyes <abrahampre27@gmail.com>
2021-07-27 22:31:58 -0500

    draggable and drop to delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var tasks = {};
 
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -185,8 +186,69 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+//////////enable draggable/sortable feature on list-group elements
 
-// load tasks for the first time
+$(".card .list-group").sortable({
+  // enable draggin across list
+  connectWith: $(".card .list-group"),
+  scroll:false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event,ui){
+    console.log("activate",this);
+  },
+  deactivate:function(event){
+    // console.log("over",event.target);
+  },
+  out: function(event){
+    // console.log("out",event.target);
+  },
+  update:function(event){
+
+    var tempArr= [];
+    //loop over current set of children in sortable list
+    $(this).children().each(function(){
+      //save values in temp array
+      var text = $(this)
+      .find ("p")
+      .text()
+      .trim();
+
+      var date= $(this)
+      .find("span")
+      .text()
+      .trim();
+      //add task data to the temp array as an object
+      tempArr.push({
+        text:text,
+        date: date
+      });
+    });
+    //trim down lists ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-","");
+
+    //update array on tasks object and save
+    tasks[arrName]=tempArr;
+    saveTasks();
+  }
+
+});
+
+$("#trash").droppable({
+  accept:".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event,ui){
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event,ui){
+    console.log("over");
+  },
+  out: function(event,ui){
+    console.log("out");
+  }
+});
+
 loadTasks();
-
-
